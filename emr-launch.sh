76c48a7c043515f4d5c244eaa026ba3772a912ce
Yuhao Zhang <yhzhang1994@gmail.com>
2019-12-02 15:37:42 -0800

    update

--- emr-launch.sh
+++ emr-launch.sh
@@ -1,55 +1,110 @@
 #!/bin/bash
-
-bid=''
-files=''
-verbose='false'
-region='us-west-2'
-label='emr-5.28.0'
-
-print_usage() {
-  printf "Usage: ..."
+set -e
+cleanup() {
+    if [ -n "$cluster_id" ]; then 
+        echo "Terminating ... cluster ID: ${cluster_id}"; 
+        aws2 emr terminate-clusters --cluster-ids "$cluster_id"
+        sleep 5
+    fi
+    exit
 }
 
-while getopts 'u:bf:vr:l:k:' flag; do
+update_script_bucket(){
+    aws2 s3api put-bucket-versioning --bucket "${s3_bucket_scripts}" --versioning-configuration Status=Enabled &&
+    aws2 s3 sync "s3://dsc102-pa2" "s3://${s3_bucket_scripts_folder}"
+}
+
+cluster_id=''
+trap cleanup INT
+
+bid=''
+region='us-west-2'
+label='emr-5.28.0'
+num_worker=4
+
+print_usage() {
+  printf "Not written yet"
+}
+
+while getopts 'u:bf:vr:l:k:n:' flag; do
   case "${flag}" in
     u) user="${OPTARG}" ;;
     b) bid='true' ;;
-    f) files="${OPTARG}" ;;
-    v) verbose='true' ;;
     r) region="${OPTARG}" ;;
     l) label="${OPTARG}" ;;
     k) key="${OPTARG}" ;;
+    n) num_worker=${OPTARG} ;;
     *) print_usage
        exit 1 ;;
   esac
 done
 
-s3_bucket=${user}-emr-logs
+s3_bucket_logs=${user}-emr-logs
+s3_bucket_scripts=${user}-pa2
+s3_bucket_scripts_folder=${s3_bucket_scripts}/src
 emr_cluster=${user}-emr-cluster
 if [[ -n "$user" ]]; then
-    if aws2 s3api head-bucket --bucket "$s3_bucket" 2>/dev/null; then
+    if aws2 s3api head-bucket --bucket "$s3_bucket_logs" 2>/dev/null; then
         echo "emr logs bucket already exists, skipping"
     else
         echo "creating emr logs bucket"
-        aws2 s3api create-bucket --acl private --bucket "${s3_bucket}" --region "${region}" --create-bucket-configuration LocationConstraint="${region}"
+        aws2 s3api create-bucket --acl private --bucket "${s3_bucket_logs}" --region "${region}" --create-bucket-configuration LocationConstraint="${region}"
+    fi
+    if aws2 s3api head-bucket --bucket "$s3_bucket_scripts" 2>/dev/null; then
+        echo "emr scripts bucket already exists, updating the bucket with missing files"
+        update_script_bucket
+    else
+        echo "creating emr scripts bucket"
+        aws2 s3api create-bucket --acl private --bucket "${s3_bucket_scripts}" --region "${region}" --create-bucket-configuration LocationConstraint="${region}" &&
+        until [ aws2 s3api head-bucket --bucket "$s3_bucket_scripts" 2>/dev/null ]; do
+            echo "waiting for bucket setting up"
+            sleep 20
+        done
+        echo "copying pa2 scripts ..."
+        update_script_bucket
+
     fi
 else
     echo "argument error"
 fi
 
+
 if [ -n "$bid" ]; then
-    instance_group_core='InstanceCount=4,InstanceType=m5.xlarge'
+    instance_group_core="InstanceCount=$num_worker,InstanceType=m5.xlarge"
 else
-    instance_group_core='InstanceCount=4,InstanceType=m5.xlarge,BidPrice=OnDemandPrice'
+    instance_group_core="InstanceCount=$num_worker,InstanceType=m5.xlarge,BidPrice=OnDemandPrice"
 fi
 
-aws2 emr create-cluster \
+ret=$(aws2 emr create-cluster \
 --release-label "${label}" \
 --instance-groups InstanceGroupType=MASTER,InstanceCount=1,InstanceType=m5.xlarge InstanceGroupType=CORE,"$instance_group_core" \
 --use-default-roles \
---ec2-attributes KeyName="${key}" \
---applications Name=JupyterHub Name=Spark Name=Hadoop \
+--ec2-attributes KeyName="${key}",SubnetId="subnet-5ea74226"  \
+--applications Name=JupyterHub Name=Spark Name=Hadoop Name=Livy Name=Hive \
 --name="$emr_cluster" \
---log-uri s3://"${s3_bucket}"
+--log-uri s3://"${s3_bucket_logs}" \
+--bootstrap-actions Path="s3://dsc102-scripts/setup-common.sh",Args=["${user}","s3://${s3_bucket_scripts_folder}"] \
+--configurations '[{"Classification":"spark","Properties":{"maximizeResourceAllocation":"true","spark.dynamicAllocation.enabled":"false"}}]'
+) &&
+cluster_id=$(echo $ret | jq -r '.ClusterId')
+
+if [  -z "$cluster_id" ]; then
+    echo "cluster failed to start"
+    exit
+fi
+# 
+echo "Cluster starting ... cluster ID: ${cluster_id}"
+sleep 20
+state=''
+until [ "$state" = "WAITING" ]; do
+    state=$(aws2 emr describe-cluster --cluster-id "${cluster_id}" | jq -r .Cluster.Status.State)
+    echo "Current cluster state: ${state}"
+    if [[ ("${state}" = "TERMINATED") || ("${state}" = "TERMINATING") || ("${state}" = "TERMINATED_WITH_ERRORS") ]]; then
+        echo "Cluster terminated accidently. Check logs on AWS console."
+        break
+    fi
+    sleep 20
+done
+
 
 # --steps Type=CUSTOM_JAR,Name=CustomJAR,ActionOnFailure=CONTINUE,Jar=s3://"${region}".elasticmapreduce/libs/script-runner/script-runner.jar,Args=["s3://YOUR_BUCKET/YOUR_SHELL_SCRIPT.sh"]
---
+++ setup-common.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+set -e
+JUPYTER_PASSWORD=${1:-"root"}
+NOTEBOOK_DIR=${2:-"s3://YourBucketForNotebookCheckpoints/yourNotebooksFolder/"}
+
+# ---------------------------- common ------------------------------------
+
+# mount home to /mnt
+if [ ! -d /mnt/home ]; then
+  	  sudo mv /home/ /mnt/
+  	  sudo ln -s /mnt/home /home
+fi
+sudo yum update -y
+sudo yum install -y htop git ;
+sudo pip-3.6 install paramiko nltk scipy scikit-learn pandas matplotlib gensim pyspark==2.4.4 jupyter jupyter_contrib_nbextensions jupyter_nbextensions_configurator findspark;
+sudo /usr/local/bin/jupyter contrib nbextension install --system ;
+sudo /usr/local/bin/jupyter nbextensions_configurator enable --system ;
+sudo /usr/local/bin/jupyter nbextension enable code_prettify/code_prettify;
+sudo /usr/local/bin/jupyter nbextension enable execute_time/ExecuteTime;
+sudo /usr/local/bin/jupyter nbextension enable collapsible_headings/main;
+export PYSPARK_PYTHON="/usr/bin/python3.6"
+# -----------------------------------------------------------------------------
+
+# ---------------------------- master only ------------------------------------
+is_master='false'
+is_master=$(jq -r '.isMaster' "/mnt/var/lib/info/instance.json")
+
+if [ "$is_master" = "true" ]; then 
+	sudo yum install automake fuse fuse-devel gcc-c++ libcurl-devel libxml2-devel make openssl-devel -y
+
+    # extract BUCKET and FOLDER to mount from NOTEBOOK_DIR
+	NOTEBOOK_DIR="${NOTEBOOK_DIR%/}/"
+	BUCKET=$(python3.6 -c "print('$NOTEBOOK_DIR'.split('//')[1].split('/')[0])")
+	FOLDER=$(python3.6 -c "print('/'.join('$NOTEBOOK_DIR'.split('//')[1].split('/')[1:-1]))")
+
+    # install s3fs
+    sudo sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/epel.repo
+    cd /mnt
+    git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+    cd s3fs-fuse/
+    # Master version does not work
+    git checkout tags/v1.85 
+    ./autogen.sh
+    ./configure
+    make
+    sudo make install
+    sudo su -c 'echo user_allow_other >> /etc/fuse.conf'
+    mkdir -p /mnt/s3fs-cache
+    mkdir -p /mnt/$BUCKET
+    /usr/local/bin/s3fs -o iam_role=auto \
+        -o umask=0 \
+        -o dbglevel=info \
+        -o curldbg \
+        -o allow_other \
+        -o use_cache=/mnt/s3fs-cache \
+        -o no_check_certificate \
+        -o enable_noobj_cache \
+        $BUCKET \
+        /mnt/$BUCKET
+    # jupyter configs
+    mkdir -p ~/.jupyter
+    touch ls ~/.jupyter/jupyter_notebook_config.py
+    HASHED_PASSWORD=$(python3.6 -c "from notebook.auth import passwd; print(passwd('$JUPYTER_PASSWORD'))")
+    echo "c.NotebookApp.password = u'$HASHED_PASSWORD'" >> ~/.jupyter/jupyter_notebook_config.py
+    echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
+    echo "c.NotebookApp.ip = '*'" >> ~/.jupyter/jupyter_notebook_config.py
+    echo "c.NotebookApp.notebook_dir = '/mnt/$BUCKET/$FOLDER'" >> ~/.jupyter/jupyter_notebook_config.py
+    echo "c.ContentsManager.checkpoints_kwargs = {'root_dir': '.checkpoints'}" >> ~/.jupyter/jupyter_notebook_config.py
+  
+    ### Setup Jupyter deamon and launch it
+    cd ~
+    echo "Creating Jupyter Daemon"
+  
+    sudo cat <<EOF > /home/hadoop/jupyter.conf
+description "Jupyter"
+
+start on runlevel [2345]
+stop on runlevel [016]
+
+respawn
+respawn limit 0 10
+
+chdir /mnt/$BUCKET/$FOLDER
+
+script
+sudo su - hadoop > /var/log/jupyter.log 2>&1 <<BASH_SCRIPT
+      export PYSPARK_DRIVER_PYTHON=/usr/bin/python3.6
+      export PYSPARK_PYTHON=/usr/bin/python3.6
+      export JAVA_HOME="/etc/alternatives/jre"
+      jupyter notebook --log-level=INFO
+BASH_SCRIPT
+
+end script
+EOF
+  
+      sudo mv /home/hadoop/jupyter.conf /etc/init/
+      sudo chown root:root /etc/init/jupyter.conf
+      sudo initctl reload-configuration
+      # start jupyter daemon
+      echo "Starting Jupyter Daemon"
+      sudo initctl start jupyter
+
+fi
+
+# -----------------------------------------------------------------------------
+
+
+
+
+
+
